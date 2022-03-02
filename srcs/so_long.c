eca0dee7605e8888e6d82ae27b267e62c3a91acb
Takuto Suetsugu <tsuetsug@c1r29s3.42tokyo.jp>
2022-03-02 20:05:44 +0900

    エラーは出ないけど、動かせない。多分ループの問題:

--- srcs/so_long.c
+++ srcs/so_long.c
@@ -6,7 +6,7 @@
 /*   By: tsuetsug <tsuetsug@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 14:02:23 by tsuetsug          #+#    #+#             */
-/*   Updated: 2022/03/02 19:48:20 by tsuetsug         ###   ########.fr       */
+/*   Updated: 2022/03/02 20:03:59 by tsuetsug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,22 +58,22 @@ int	key_press(int keycode, t_vars *vars)
 {
 	if (keycode == KEY_W)
 	{
-		vars->move_y++;
+		vars->move_y = 1;
 		printf("KEY_W\n");
 	}
 	else if (keycode == KEY_S)
 	{
-		vars->move_y--;
+		vars->move_y = -1;
 		printf("KEY_S\n");
 	}
 	else if (keycode == KEY_D)
 	{
-		vars->move_x++;
+		vars->move_x = 1;
 		printf("KEY_D\n");
 	}
 	else if (keycode == KEY_A)
 	{
-		vars->move_x--;
+		vars->move_x = -1;
 		printf("KEY_A\n");
 	}
 	else if (keycode == KEY_ESC)
@@ -95,7 +95,7 @@ char	*search_element_addr(t_map *map, char element)
 	{
 		while (x++ < (map->col - 1))
 		{
-			if (map->content[y][x] == 'P')
+			if (map->content[y][x] == element)
 				return (&map->content[y][x]);
 		}
 		x = -1;
@@ -104,13 +104,43 @@ char	*search_element_addr(t_map *map, char element)
 	return (NULL);
 }
 
-void	key_press_proc(t_map *map, t_data *img, t_vars *vars)
+void	key_press_proc(t_map *map, t_vars *vars)
 {	
 	char	*P_addr;
 	char	*E_addr;
 
-	P_addr = search_element_addr(map, "P");
-	E_addr = search_element_addr(map, "E");
+	P_addr = search_element_addr(map, 'P');
+	E_addr = search_element_addr(map, 'E');
+
+	if (vars->move_x == 1)
+	{
+		*P_addr = '0';
+		*(P_addr + 1) = 'P';
+	}
+	else if (vars->move_x == -1)
+	{
+		*P_addr = '0';
+		*(P_addr - 1) = 'P';
+	}
+	else if (vars->move_y == 1)
+	{
+		*P_addr = '0';
+		*(P_addr + map->col) = 'P';
+	}
+	else if (vars->move_y == -1)
+	{
+		*P_addr = '0';
+		*(P_addr - map->col) = 'P';
+	}
+}
+
+void	vars_init(t_vars *vars)
+{
+	vars->mlx = mlx_init();
+	if (vars->mlx == NULL)
+		ft_error("mlx is not valid");
+	vars->move_x = 0;
+	vars->move_y = 0;
 }
 
 
@@ -122,18 +152,14 @@ int	main(int argc, char **argv)
 
 	if (argc != 2)
 		ft_error("argc is not 2");
-	vars.mlx = mlx_init();
-	if (vars.mlx == NULL)
-		ft_error("mlx is not valid");
+	vars_init(&vars);
 	vars.win = mlx_new_window(vars.mlx, 1000, 400, "so_long");
 	download_images(&vars, &img);
 	import_map(argv[1], &map);
 	print_map(&map, &img, &vars);
 
 	mlx_hook(vars.win, 2, 1L<<0, key_press, &vars);
-	key_press_proc(&map, &img, &vars);
-	
-	print_map(&map, &img, &vars);
+	key_press_proc(&map, &vars);
 
 	mlx_loop(vars.mlx);
 	return (0);
